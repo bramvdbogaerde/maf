fe7fa0f4a70e1d66a32cddd46228a2ce5d5337e1
bramvdbogaerde <contact@bramvdb.com>
2021-05-27 19:51:04 +0200

    benchfix: indentation recip.rkt

--- test/soft-contract/NguyenGTH18/safe/sym-exe/recip.rkt
+++ test/soft-contract/NguyenGTH18/safe/sym-exe/recip.rkt
@@ -2,12 +2,12 @@
 ;; under analysis such that we can re-use the path conditions contained within
 
 (define (recip x)
-  (if (and (int? x) (not (zero? x)))
+  (if (and (number? x) (not (zero? x)))
       (/ 1 x)
       "expect non-zero number"))
 
 (provide/contract 
-  (recip (-> any/c (or/c (and/c int? (not/c zero?)) string?))))
+  (recip (-> any/c (or/c (and/c number? (not/c zero?)) string?))))
 
 (@unchecked recip OPQ)
 (safe)
