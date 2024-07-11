3eeeeda4be6772a54beb8b34901e18f5968e4029
Bram Vandenbogaerde <bram@vdb.space>
2024-07-11 23:19:36 +0200

    fix: string->number to return #f when input is not a valid representation of a number

--- code/shared/src/main/scala/maf/language/scheme/interpreter/ConcreteSchemePrimitives.scala
+++ code/shared/src/main/scala/maf/language/scheme/interpreter/ConcreteSchemePrimitives.scala
@@ -924,7 +924,8 @@ trait ConcreteSchemePrimitives:
             def fun = { case Value.Pointer(addr) =>
                 val str = getString(addr)
                 if str.toIntOption.nonEmpty then Value.Integer(str.toIntOption.get)
-                else signalException(s"$name: $str can not be converted into a number")
+                // R5RS: "if string is not a valid notation for a number, then string->number returns #f"
+                else Value.Bool(false)
             }
 
         object Substring extends Prim:
