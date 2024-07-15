78e65e4a1c45f2bbba30d0d9902660025a9466bb
Bram Vandenbogaerde <bram@vdb.space>
2024-07-15 11:06:26 +0200

    dd: print actual strings being compared + test dderiv

--- code/jvm/src/main/scala/maf/cli/experiments/delta/DeltaDebugEvaluation.scala
+++ code/jvm/src/main/scala/maf/cli/experiments/delta/DeltaDebugEvaluation.scala
@@ -194,7 +194,7 @@ class PrintBasedInterpreterComparison extends InterpreterComparison:
           runInternalInterpreter(program),
           runExternalInterpreter(program),
           (v1: String, v2: String) =>
-              println(s"comparing output $v1 with $v2")
+              println(s"comparing output ${processOutput(v1)} with ${processOutput(v2)}")
               if processOutput(v1) == processOutput(v2) then None else Some(OutputDisagreement(name, processOutput(v1), processOutput(v2)))
         )
 
@@ -793,12 +793,12 @@ object Evaluation:
     val benchmarks: Set[String] = Set(
       // These are all the ones that yield differences worth investigating
       // Different order of evaluation of let bindings?
-      //"test/R5RS/gabriel/dderiv.scm", // (let ((arg ((lambda unique_args_382 #f) 5 '())) (result ((lambda unique_args_374 '()) 0 '()))) (equal? '() result))
+      "test/R5RS/gabriel/dderiv.scm", // (let ((arg ((lambda unique_args_382 #f) 5 '())) (result ((lambda unique_args_374 '()) 0 '()))) (equal? '() result))
       //"test/R5RS/scp1/cashdesk-counter.scm", // (letrec ((teller ((lambda unique_args_295 #f))) (_0 ((lambda unique_args_287 '()) 'toets)) (_3 teller)) '())
       //"test/R5RS/scp1/twitter.scm", // (letrec ((res1 ((lambda unique_args_463 #f) 'username)) (_0 ((lambda unique_args_455 '()) 'output)) (_6 res1)) '())
       ////
       //// Bug: (eq?) and (eq? x) are valid in guile, but not in MAF. It's guile that deviates from R5RS
-      "test/R5RS/various/values.scm", // (letrec ((string->number eq?) (_1 (string->number))) '())
+      //"test/R5RS/various/values.scm", // (letrec ((string->number eq?) (_1 (string->number))) '())
       ////
       //// Bug: letrec can reference later bindings in the same letrec, does not work in MAF
       //// It's actually guile that violates R5RS, as it states: "One restriction on letrec is very important: it must be possible to evaluate each <init> without assigning or referring to the value of any <variable>. If this restriction is violated, then it is an error. The restriction is necessary because Scheme passes arguments by value rather than by name. In the most common uses of letrec, all the <init>s are lambda expressions and the restriction is satisfied automatically. "
